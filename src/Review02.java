b0d6b96f15c412b990476e7e9964a1047a3b1390
HikaruIzuno <izuno.hikaru@linecorp.com>
2024-09-27 15:14:00 +0900

    ブランチ名称の変更と説明文も追加

--- src/Review02.java
+++ src/Review02.java
@@ -4,16 +4,16 @@ public class Review02 {
     public static void main(String[] args) {
         //1~100までの繰り返し操作
         for (int i = 1; i <= 100; i++) {
+            //3,5いづれでも割り切れるとき
             if(i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
-            }else if(i % 3 == 0 ) {
+            }else if(i % 3 == 0 ) { //3で割り切れる時
                 System.out.println("Fizz");
-            }else if(i % 5 == 0 ) {
+            }else if(i % 5 == 0 ) { //5で割り切れる時
                 System.out.println("Buzz");
             }
             else {
                 System.out.println(i);
-
             }
         }
 
